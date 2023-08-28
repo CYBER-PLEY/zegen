2f76a41160abba211dc7884cd603746dd1f50f37
Prince Lawson <princelawson691@gmail.com>
2023-08-28 15:36:38 +0100

    almost done

---
+++ test.js
@@ -0,0 +1,9 @@
+// console.log('hello world');
+// let name ='marsh';
+// console.log(name);
+// let firstName ='Prince';
+// let lastName = 'Lawson';
+// console.log(firstName,lastName)
+let name = 'Prince';
+let age = '17';
+let isapproved = true;
