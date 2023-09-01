0abd6951d21f19cb617b361f2044d5fac9165cfb
Prince Lawson <princelawson691@gmail.com>
2023-09-01 10:22:44 +0100

    modified

--- test.js
+++ test.js
@@ -6,4 +6,69 @@
 // console.log(firstName,lastName)
 let name = 'Prince';
 let age = '17';
-let isapproved = true;
+let isapproved = true
+var firstAge= 18
+var secondAge=1
+if (secondAge = firstAge)
+console.log(name+" can drink")
+else 
+console.log(name+" cannot drink")
+;
+document.querySelector("div").style.fontSize="30px";
+document.querySelector("div").style.color="blue"
+
+// var names =[ "abia", 1.2 , "umuaiaha", "adeleke"];
+
+// console.log(names.length[4]);
+
+
+// const services = [
+//     {
+//       id: 1,
+//       title: "DESIGN AND SURVEYING",
+//       description:
+//         "Meticulous design and surveying services to ensure precision and adherence to industry standards."
+//     },
+//     {
+//       id: 2,
+//       title: "CONSTRUCTION OF BUILDINGS, ROADS, AND BRIDGES",
+//       description:
+//         "Expertise in constructing high-quality buildings, roads, and bridges with a focus on durability and functionality."
+//     },
+//     {
+//       id: 3,
+//       title: "GEOTECHNICAL INVESTIGATION",
+//       description:
+//         "Thorough geotechnical investigations to assess soil conditions and provide valuable insights for foundation design and construction"
+//     }
+//   ];
+  
+//   const serviceContainer = document.getElementById("services");
+  
+//   services.forEach(service => {
+//     const serviceElement = document.createElement("div");
+  
+//     serviceElement.innerHTML = `
+//       <p>${service.id}</p>
+//       <p>${service.title}</p>
+//       <p>${service.description}</p>
+//     `;
+//     serviceContainer.appendChild(serviceElement);
+//   });
+  
+// function description(){
+//   var name="prince";
+//   const age=16
+//  alert( "my name is "+name+" i am "+age)
+// }
+// description()
+
+// // // var fruits=["banana","orange","apple","pineapple"]
+
+// // // console.log(fruits[0])
+// // for(var i=0 ; i<=100; i++){
+// //   if (i % 3 == 0)
+// //   {
+// //     console.log(i + " multiple of 3")
+// //   }
+// // }
